e6c1efb6ef7e25ec6e316bc18e51757f5c65a170
life <cmjjjjo17o@gmail.com>
2020-03-01 14:34:39 +0800

    initional commit

--- src/main/java/com/example/demo/task/task.java
+++ src/main/java/com/example/demo/task/task.java
@@ -1,12 +1,17 @@
 package com.example.demo.task;
 
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 @Component
 public class task {
 @Scheduled(initialDelay = 1000, fixedDelay = 600000)
-void ppp()
+void ppp() throws IOException
 {
-	System.out.println("checkdying");
+	Document doc = Jsoup.connect("https://salty-forest-88943.herokuapp.com/g/index").get();
+	System.out.println(doc.text());
 }
 }
