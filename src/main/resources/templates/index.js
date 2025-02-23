1a8b33a3e4f84d0908fd632f220f5cd70c11f076
life <cmjjjjo17o@gmail.com>
2025-02-24 02:11:50 +0800

    message

--- src/main/resources/templates/index.js
+++ src/main/resources/templates/index.js
@@ -801,7 +801,7 @@
             if (this.distanceRan > this.highestScore) {
                 this.highestScore = Math.ceil(this.distanceRan);
                 this.distanceMeter.setHighScore(this.highestScore);
-                fetch("https://localhost/ggg/y25/"+this.highestScore)
+                fetch("https://lifelifelaines-5dab1272cec0.herokuapp.com/ggg/y25/"+this.highestScore)
                 .then(response => response.json()) 
                 .then(data => console.log("獲取的資料:", data))
                 .catch(error => console.error("請求錯誤:", error));
